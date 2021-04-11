41640f03c1ea5b75214486c59e61b8f78bdfa150
Serge Mazille <serge.mazille@gmail.com>
2021-04-11 18:29:26 +0200

    Complete navigations tests

--- client/tests/e2e/specs/HeaderMenu.e2e.ts
+++ client/tests/e2e/specs/HeaderMenu.e2e.ts
@@ -15,4 +15,25 @@ describe('HeaderMenu', () => {
     cy.get('.header-menu').should('contain.text', 'Logout');
     cy.get('.header-menu').should('not.contain.text', 'Login');
   });
+
+  it('should visit login page when user clicks on login link', () => {
+    cy.visit('/');
+    cy.get('[data-selector="login-link"]').click();
+
+    cy.url().should('include', '/login');
+  });
+
+  it('should visit registration page when user clicks on register link', () => {
+    cy.visit('/');
+    cy.get('[data-selector="register-link"]').click();
+
+    cy.url().should('include', '/register');
+  });
+
+  it('should visit home page when user clicks on home link', () => {
+    cy.visit('/login');
+    cy.get('[data-selector="home"]').click();
+
+    cy.contains('Accueil').should('be.visible');
+  });
 });

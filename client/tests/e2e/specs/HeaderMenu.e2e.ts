31e80ade626ceb6388617e62a768ca712beb3114
Serge Mazille <serge.mazille@gmail.com>
2021-04-11 17:57:20 +0200

    Test when user is logged in

--- client/tests/e2e/specs/HeaderMenu.e2e.ts
+++ client/tests/e2e/specs/HeaderMenu.e2e.ts
@@ -1,9 +1,18 @@
 describe('HeaderMenu', () => {
   it('display the header menu while not logged in', () => {
     cy.visit('/');
+    cy.userIsAuthenticated(false);
 
     cy.get('.header-menu').should('contain.text', 'Accueil');
     cy.get('.header-menu').should('contain.text', 'Login');
     cy.get('.header-menu').should('contain.text', 'Register');
+  });
+
+  it('display the header menu while logged in', () => {
+    cy.visit('/');
+    cy.userIsAuthenticated(true);
+
+    cy.get('.header-menu').should('contain.text', 'Logout');
+    cy.get('.header-menu').should('not.contain.text', 'Login');
   });
 });

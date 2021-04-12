026dbd2276955a25fce368d326f673731e4fbee6
Serge Mazille <serge.mazille@gmail.com>
2021-04-12 07:57:25 +0200

    Add UserAuthForm e2e tests

--- client/tests/e2e/specs/UserAuthForm.e2e.ts
+++ client/tests/e2e/specs/UserAuthForm.e2e.ts
@@ -1,5 +1,34 @@
 describe('UserAuthForm', () => {
-  it('visit the login page', () => {
+  it('submit button is disabled as a default state', () => {
     cy.visit('/login');
+
+    cy.get('button').should('have.attr', 'disabled', 'disabled');
+  });
+
+  it('submit button is enabled when inputs are valid', () => {
+    cy.visit('/login');
+
+    cy.get('input[type="email"]').type('user@email.com');
+    cy.get('input[type="password"]').type('password');
+
+    cy.get('button').should('not.have.attr', 'disabled');
+  });
+
+  it('submit button is disabled if email is not a valid email', () => {
+    cy.visit('/login');
+
+    cy.get('input[type="email"]').type('not a valid email');
+    cy.get('input[type="password"]').type('password');
+
+    cy.get('button').should('have.attr', 'disabled', 'disabled');
+  });
+
+  it('submit button is disabled if password has less than 4 characters', () => {
+    cy.visit('/login');
+
+    cy.get('input[type="email"]').type('user@email.com');
+    cy.get('input[type="password"]').type('abc');
+
+    cy.get('button').should('have.attr', 'disabled', 'disabled');
   });
 });

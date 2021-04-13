bbcbd6c3c7c3e3d81b99fe03c7115e3a4acfc99d
Serge Mazille <serge.mazille@gmail.com>
2021-04-13 10:02:43 +0200

    Add authentication redirection tests

--- client/tests/e2e/specs/Dashboard.e2e.ts
+++ client/tests/e2e/specs/Dashboard.e2e.ts
@@ -1,7 +1,25 @@
 describe('Dashboard', () => {
-  it('visit the dashboard page', () => {
+  it('visit the dashboard page while authenticated', () => {
     cy.userIsAuthenticated(true);
     cy.visit('/dashboard');
+
+    cy.contains('Dashboard');
+  });
+
+  it('should be redirected to login page if not authenticated', () => {
+    cy.userIsAuthenticated(false);
+    cy.visit('/dashboard');
+
+    cy.url().should('include', '/login');
+  });
+
+  it('should be redirected to dashboard page after authentication', () => {
+    cy.userIsAuthenticated(false);
+    cy.visit('/dashboard');
+
+    cy.get('input[type="email"]').type('user@email.com');
+    cy.get('input[type="password"]').type('password');
+    cy.get('button').click();
 
     cy.contains('Dashboard');
   });
--- client/tests/e2e/support/commands.ts
+++ client/tests/e2e/support/commands.ts
@@ -1,37 +1,18 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
-Cypress.Commands.add('userIsAuthenticated', isAuthenticated => {
+Cypress.Commands.add('userIsAuthenticated', doAuthenticated => {
   cy.visit('/');
 
   cy.window()
     .its('store')
     .then(store => {
-      store.state.auth.isAuthenticated = isAuthenticated;
-      localStorage.setItem('isAuthenticated', JSON.stringify(true));
+      // in memory store
+      store.state.auth.isAuthenticated = doAuthenticated;
+
+      // local store
+      if (doAuthenticated) {
+        localStorage.setItem('isAuthenticated', JSON.stringify(doAuthenticated));
+      } else {
+        localStorage.removeItem('isAuthenticated');
+      }
     });
 
   cy.wait(0);

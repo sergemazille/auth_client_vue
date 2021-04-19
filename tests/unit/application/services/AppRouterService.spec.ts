3d5beb8a4fd9c3b04fa38a6dced201d993b2ef7f
Serge Mazille <serge.mazille@gmail.com>
2021-04-19 08:01:27 +0200

    Test guarded routes

--- tests/unit/application/services/AppRouterService.spec.ts
+++ tests/unit/application/services/AppRouterService.spec.ts
@@ -2,15 +2,46 @@ import { RouteName, VueRouterFactory } from '@/infrastructure/routing/VueRouterF
 import { AppRouterService } from '@/application/services/AppRouterService';
 import { routes } from '@/infrastructure/routing/routes';
 
-const authService: any = jest.fn();
-const { router } = new VueRouterFactory(routes, authService);
-
 describe('AppRouterService', () => {
   it('should return actual route name', async () => {
+    const authService: any = jest.fn();
+    const { router } = new VueRouterFactory(routes, authService);
     const routerService = new AppRouterService(router);
 
     await router.push({ name: RouteName.LOGIN });
 
     expect(routerService.currentRouteName).toBe(RouteName.LOGIN);
+  });
+
+  it('should redirect to login page if user is not authenticated before visiting a guarded page', async () => {
+    routes.push({
+      path: '',
+      component: {} as any,
+      name: 'GUARDED_ROUTE' as RouteName,
+      meta: { requiresAuth: true },
+    });
+    const authService: any = { isAuthenticated: false };
+    const { router } = new VueRouterFactory(routes, authService);
+    const routerService = new AppRouterService(router);
+
+    await router.push({ name: 'GUARDED_ROUTE' });
+
+    expect(routerService.currentRouteName).toBe(RouteName.LOGIN);
+  });
+
+  it('should visit requested page if user is authenticated before visiting a guarded page', async () => {
+    routes.push({
+      path: '',
+      component: {} as any,
+      name: 'GUARDED_ROUTE' as RouteName,
+      meta: { requiresAuth: true },
+    });
+    const authService: any = { isAuthenticated: true };
+    const { router } = new VueRouterFactory(routes, authService);
+    const routerService = new AppRouterService(router);
+
+    await router.push({ name: 'GUARDED_ROUTE' });
+
+    expect(routerService.currentRouteName).toBe('GUARDED_ROUTE');
   });
 });

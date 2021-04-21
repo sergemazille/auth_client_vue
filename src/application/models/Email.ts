82cbb0d9bd5d247fa0c7faaa19ed917f9b1cc832
Serge Mazille <serge.mazille@gmail.com>
2021-04-21 08:17:15 +0200

    Add "asString" getter

--- src/application/models/Email.ts
+++ src/application/models/Email.ts
@@ -1,17 +1,17 @@
 export class Email {
   static readonly REGEXP = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  value: string;
-
-  constructor(email: string) {
+  constructor(private readonly email: string) {
     if (!Email.isValid(email)) {
       throw new Error('Email is not valid');
     }
-
-    this.value = email;
   }
 
   static isValid(email: string): boolean {
     return !!email.toLowerCase().match(Email.REGEXP);
+  }
+
+  asString(): string {
+    return this.email;
   }
 }

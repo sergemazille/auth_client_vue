9747a23da4549dd4748817f51d42dd2449dcb8d2
Serge Mazille <serge.mazille@gmail.com>
2021-04-24 17:17:08 +0200

    Create auth navigation own file

--- tests/e2e/specs/AuthForm.e2e.ts
+++ tests/e2e/specs/AuthForm.e2e.ts
@@ -31,32 +31,4 @@ describe('AuthForm', () => {
 
     cy.get('button').should('have.attr', 'disabled', 'disabled');
   });
-
-  it("should redirect to a secured page if api call's response is OK", () => {
-    cy.visit('/login?redirect=/dashboard');
-
-    cy.intercept('POST', '**/api/v1/login', {
-      statusCode: 200, // success
-    }).as('loginRequest');
-
-    cy.get('input[type="email"]').type('user@email.com');
-    cy.get('input[type="password"]').type('abcd{enter}');
-    cy.wait('@loginRequest');
-
-    cy.contains('Accès autorisé uniquement à un utilisateur authentifié').should('be.visible');
-  });
-
-  it("should not redirect to a secured page if api call's response is not OK", () => {
-    cy.visit('/login?redirect=/dashboard');
-
-    cy.intercept('POST', '**/api/v1/login', {
-      statusCode: 401, // failure
-    }).as('loginRequest');
-
-    cy.get('input[type="email"]').type('user@email.com');
-    cy.get('input[type="password"]').type('abcd{enter}');
-    cy.wait('@loginRequest');
-
-    cy.contains('Accès autorisé uniquement à un utilisateur authentifié').should('not.exist');
-  });
 });

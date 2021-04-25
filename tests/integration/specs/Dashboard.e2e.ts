dcd6d60e3082770c684f191a4df97884026ca41a
Serge Mazille <serge.mazille@gmail.com>
2021-04-25 19:24:55 +0200

    Fix false negative

--- tests/integration/specs/Dashboard.e2e.ts
+++ tests/integration/specs/Dashboard.e2e.ts
@@ -17,10 +17,14 @@ describe('Dashboard', () => {
     cy.userIsAuthenticated(false);
     cy.visit('/dashboard');
 
+    cy.intercept('POST', '**/api/v1/login', {
+      statusCode: 200, // success
+    }).as('loginRequest');
+
     cy.get('input[type="email"]').type('user@email.com');
     cy.get('input[type="password"]').type('password');
     cy.get('button').click();
 
-    cy.contains('Dashboard');
+    cy.contains('Accès autorisé uniquement à un utilisateur authentifié').should('be.visible');
   });
 });
